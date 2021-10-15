3d41e57cf40f90444c6fc9605545f0dbeb446e95
MysteryMS <38635037+MysteryMS@users.noreply.github.com>
2021-10-14 21:22:11 -0300

    semicolons

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const app = express();
 app.use(express.static('views'))
 
 app.get('/', (req, res) => {
-  res.send('index')
+  res.send('index');
 });
 
 
 //status codes
 app.get('/100', function (req, res) {
 res.sendFile(path.resolve('./img/100.png'));
-}) 
+}) ;
 
 app.get('/200', function (req, res) {
 res.sendFile(path.resolve('/img/200.png'));
-}) 
+}); 
 
 app.get("/404", (req, res) => {
   res.redirect('');
-})
+});
 
 app.listen(80, () => {
   console.clear()
   console.log(`[APP] Iniciado com sucesso.`)
-})
+});
