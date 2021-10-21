cbe164c337b50da991c2b4c942c0ae5bbf31a925
kayake <77542672+kayke981@users.noreply.github.com>
2021-10-20 22:59:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+const {resolve} = require('path');
 const app = express();
 
 app.use(express.static('views'))
@@ -10,16 +10,17 @@ app.get('/', (req, res) => {
 
 
 //status codes
+
 app.get('/100', function(req, res) {
-  res.sendFile(path.resolve('./public/img/100.png'));
+  res.sendFile(resolve('./public/img/100.png'));
 });
 
 app.get('/200', function(req, res) {
-  res.sendFile(path.resolve('./public/img/200.png'));
+  res.sendFile(resolve('./public/img/200.png'));
 });
 
 app.get('/413', function(req, res) {
-  res.sendFile(path.resolve('./public/img/413.png'));
+  res.sendFile(resolve('./public/img/413.png'));
 });
 
 app.use(express.static(__dirname + "/public"), (_, res, next) => {
